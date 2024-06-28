d829bb09d1c61085f439ce3e84802ea8c68de1eb
sahana2305 <152769241+sahana2305@users.noreply.github.com>
2024-06-28 15:22:49 +0530

    Add files via upload

---
+++ n even.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,n,flag;
+scanf("%d",&n);
+i=1;
+flag=0;
+while(i<=2*n)
+    {
+        if(i%2==0)
+        {
+            if(flag==0){
+                    printf("%d",i);
+                    flag=1;
+                    }
+                    else{
+                            printf(",%d",i);
+                    }
+                    }
+                    i++;
+                    }
+                    }
